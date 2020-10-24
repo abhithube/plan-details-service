96fc38f4afdd1cfcc23ef3c28b2d94e5d4f787e3
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-23 20:43:54 -0700

    Added support for API versioning

--- src/main/java/io/abhithube/plandetailsservice/controller/PlanController.java
+++ src/main/java/io/abhithube/plandetailsservice/controller/PlanController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/plans")
+@RequestMapping("/api/v1/plans")
 public class PlanController {
     private final PlanService planService;
 

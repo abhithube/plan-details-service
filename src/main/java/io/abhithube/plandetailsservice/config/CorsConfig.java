21784edd40b87ff9f5356cdbf726aff8b4011a1d
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-25 20:17:37 -0700

    Updated CORS config to allow Angular load balancer as an origin

--- src/main/java/io/abhithube/plandetailsservice/config/CorsConfig.java
+++ src/main/java/io/abhithube/plandetailsservice/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200");
+                .allowedOrigins("https://app.at-insurance.com");
     }
 }
